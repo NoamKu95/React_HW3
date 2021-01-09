09e107401d485242f8e5bd1615921ecaf0984ce8
Noam Kurtzer <68505926+NoamKu95@users.noreply.github.com>
2021-01-09 21:04:10 +0200

    Adding DB and Server side files

---
+++ API/WebApi/Controllers/IngredientsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DATA.EF;
+
+namespace WebApi.Controllers
+{
+    public class IngredientsController : ApiController
+    {
+        // GET api/<controller>
+        public dynamic Get()
+        {
+
+            KitchenDbContext db = new KitchenDbContext();
+            var ingredients = db.Ingredients.Select(x => new
+            {
+                IngredientName = x.name,
+                IngredientImage = x.image,
+                IngredientId = x.id,
+                Calories = x.calories
+            }).ToList();
+            return ingredients;
+        }
+
+        // GET api/<controller>/5
+        public dynamic Get(int id)
+        {
+            KitchenDbContext db = new KitchenDbContext();
+            var i = db.Ingredients.Select(ing => new
+            {
+                IngredientName = ing.name,
+                IngredientImage = ing.image,
+                IngredientId = ing.id,
+                Calories = ing.calories
+            }).SingleOrDefault(x => x.IngredientId == id);
+
+            if (i != null)
+            {
+                return i;
+            }
+            return null;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Ingredient value)
+        {
+            KitchenDbContext db = new KitchenDbContext();
+
+            Ingredient i = new Ingredient();
+            i.name = value.name;
+            i.image = value.image;
+            i.calories = value.calories;
+
+            db.Ingredients.Add(i);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ API/WebApi/Controllers/RecipesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DATA.EF;
+
+namespace WebApi.Controllers
+{
+    public class RecipesController : ApiController
+    {
+        // GET api/<controller>
+        public dynamic Get()
+        {
+            KitchenDbContext db = new KitchenDbContext();
+            var recipes = db.Recipes.Select(x => new
+            {
+                
+                RecipeName = x.name,
+                RecipeImage = x.image,
+                RecipeId = x.id,
+                CookingTime = x.time,
+                CookingMethod = x.cookingMethod
+            }).ToList();
+
+            return recipes;
+        }
+
+
+        // POST api/<controller>
+        public void Post([FromBody]Recipe value)
+        {
+            KitchenDbContext db = new KitchenDbContext();
+            try
+            {
+                Recipe r = new Recipe();
+                r.name = value.name;
+                r.image = value.image;
+                r.time = value.time;
+                r.cookingMethod = value.cookingMethod;
+
+                r.Ingredients = value.Ingredients; //What do we get in "Ingredients" from the client?
+
+                db.Recipes.Add(r);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+            }
+            
+        }
+
+
+    }
+}
